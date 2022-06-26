a2342e710c9973210e0499b9d524e34078783f4a
Guilherme Santos <guilhermess1994@gmail.com>
2022-06-26 20:37:08 -0300

    initial commit

---
+++ src/Component.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let name;
+  export let buttonText = "Button";
+
+  function handleClick() {
+    buttonText += " Clicked";
+  }
+</script>
+
+<svelte:options tag="my-counter" />
+
+<!-- your code here -->
+<h1>Hello {name}!</h1>
+
+<button on:click={handleClick}>{buttonText}</button>
